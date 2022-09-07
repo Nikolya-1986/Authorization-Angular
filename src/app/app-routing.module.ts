3c1e32ac4e694125466f5423e175e993034918d8
Nikolai Amosov <nikolai_sergeevich@mail.ru>
2022-09-07 15:14:44 +0300

    canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   {
-    path: 'home', component: HomeComponent
+    path: 'home', canActivate:[AuthGuardService], component: HomeComponent
   },
   {
     path: 'login', component: LoginComponent
